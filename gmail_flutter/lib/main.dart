38081ed498e97c45f8909c65d7992deffa9c0634
8igmac <8igmac@pm.me>
2023-03-17 09:29:49 +0800

    [UI-FEAT] Fine-tune home page UI.

--- gmail_flutter/lib/main.dart
+++ gmail_flutter/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -13,6 +13,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: true,
+        colorSchemeSeed: Colors.amber, // note: this is cool
+      ),
       home: const HomePage(),
     );
   }
@@ -27,7 +31,7 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   int _currentIndex = 0;
-  String _imageAddr =
+  final String _imageAddr =
       'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ9r3ogaSmpwNYSaEKRifVaHjwmYsKSW7fC6Q&usqp=CAU';
 
   @override
@@ -35,43 +39,61 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       bottomNavigationBar: NavigationBar(
         selectedIndex: _currentIndex,
+        height: 60,
+        labelBehavior: NavigationDestinationLabelBehavior.alwaysHide,
         onDestinationSelected: (int index) {
           setState(() {
             _currentIndex = index;
           });
         },
-        destinations: [
+        destinations: const [
           NavigationDestination(
             icon: Icon(Icons.mail_outline),
+            selectedIcon: Icon(Icons.mail),
             label: 'mail',
           ),
           NavigationDestination(
             icon: Icon(Icons.chat_bubble_outline),
+            selectedIcon: Icon(Icons.chat_bubble),
             label: 'chat',
           ),
           NavigationDestination(
             icon: Icon(Icons.group_outlined),
+            selectedIcon: Icon(Icons.group),
             label: 'group',
           ),
           NavigationDestination(
             icon: Icon(Icons.videocam_outlined),
+            selectedIcon: Icon(Icons.videocam),
             label: 'video call',
           ),
         ],
       ),
       appBar: AppBar(
-        title: Text('Search'),
+        title: const Text('Search'),
+        elevation: 22,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
         actions: [
           CircleAvatar(
             backgroundImage: NetworkImage(_imageAddr),
           ),
+          const SizedBox(width: 10),
         ],
       ),
       drawer: Drawer(
         child: ListView(
-          children: [
-            Text(
-              "Gmail",
+          children: const [
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Text(
+                "Gmail",
+                style: TextStyle(
+                  fontSize: 30,
+                  color: Colors.red,
+                ),
+              ),
             ),
             Divider(),
             ListTile(
@@ -105,37 +127,45 @@ class _HomePageState extends State<HomePage> {
         itemCount: 50,
         itemBuilder: (context, i) {
           return ListTile(
+            contentPadding: const EdgeInsets.all(10),
             leading: CircleAvatar(
+              radius: 40,
               backgroundImage: NetworkImage(_imageAddr),
             ),
             title: Row(
-              children: [
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: const [
                 Text('Title'),
                 Text("3月8號"),
               ],
             ),
             subtitle: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 Column(
-                  children: [
+                  children: const [
                     Text(
                       'subtitlasdfsdfadsfe',
+                      style: TextStyle(fontSize: 20),
                     ),
                     Text(
                       'subtitlasdfsdfadsfe',
+                      style: TextStyle(fontSize: 20),
                     ),
                   ],
                 ),
-                Icon(Icons.star_outline),
+                const Icon(Icons.star_outline),
               ],
             ),
           );
         },
       ),
-      floatingActionButton: FloatingActionButton.extended(
+      floatingActionButton: const FloatingActionButton.extended(
         icon: Icon(Icons.edit_outlined),
         label: Text('Edit'),
         onPressed: null,
+        extendedIconLabelSpacing: 30,
+        extendedPadding: EdgeInsets.symmetric(horizontal: 20),
       ),
     );
   }
